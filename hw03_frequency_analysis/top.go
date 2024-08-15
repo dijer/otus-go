4faf23b8764ba6282335972a4768f44b315ba537
Timur Akhmetzyanov <dijer@mail.ru>
2024-08-15 22:59:37 +0300

    HW03 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,39 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var regexpValid = regexp.MustCompile(`^[0-9A-Za-zа-яА-Я_,-]+$`)
+
+func Top10(text string) []string {
+	countMap := map[string]int{}
+	textArr := strings.Fields(text)
+	outputArr := []string{}
+
+	for _, word := range textArr {
+		if !regexpValid.MatchString(word) {
+			continue
+		}
+		if countMap[word] == 0 {
+			outputArr = append(outputArr, word)
+		}
+		countMap[word]++
+	}
+
+	sort.Slice(outputArr, func(i, j int) bool {
+		if countMap[outputArr[i]] == countMap[outputArr[j]] {
+			return strings.Compare(outputArr[i], outputArr[j]) < 0
+		}
+		return countMap[outputArr[i]] > countMap[outputArr[j]]
+	})
+
+	max := 10
+	if max > len(outputArr) {
+		max = len(outputArr)
+	}
+
+	return outputArr[0:max]
 }
