b3fae62ec196e562ca010264ca3678bbc06d5f6a
Timur Akhmetzyanov <dijer@mail.ru>
2024-10-05 22:25:05 +0300

    hw08_envdir_tool completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,42 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
+const (
+	errCode     = 1
+	successCode = 0
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	for envName, envValue := range env {
+		if envValue.NeedRemove {
+			err := os.Unsetenv(envName)
+			if err != nil {
+				return errCode
+			}
+			continue
+		}
+
+		err := os.Setenv(envName, envValue.Value)
+		if err != nil {
+			return errCode
+		}
+	}
+
+	cmdCommand, cmdArgs := cmd[0], cmd[1:]
+	command := exec.Command(cmdCommand, cmdArgs...)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Stdin = os.Stdin
+
+	err := command.Run()
+	if err != nil {
+		return command.ProcessState.ExitCode()
+	}
+
+	return successCode
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+const minArgs = 2
+
 func main() {
-	// Place your code here.
+	args := os.Args[1:]
+	if len(args) < minArgs {
+		fmt.Println("low args")
+		return
+	}
+
+	dir, cmd := args[0], args[1:]
+	envs, err := ReadDir(dir)
+	if err != nil {
+		fmt.Printf("err %v", err)
+		return
+	}
+
+	exit := RunCmd(cmd, envs)
+	os.Exit(exit)
 }
